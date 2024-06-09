a600bd5ab1fffa176d59a08582aa8e1429c7ddcc
HANUMANT HAIBATI DOMBALE <150545489+hanumant-dombale@users.noreply.github.com>
2024-06-09 17:25:10 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.querySelector('.nav-list');
+
+    mobileMenu.addEventListener('click', () => {
+        navList.classList.toggle('active');
+    });
+});
+
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Set default open tab
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.querySelector('.tablinks').click();
+});
